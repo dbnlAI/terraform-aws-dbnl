b94e950b23f4411c326301dfcdb975fbe785473e
dbnl-dan <dan@distributional.com>
2026-03-25 11:11:53 -0400

    remove clunky

--- examples/oidc/outputs.tf
+++ examples/oidc/outputs.tf
@@ -7,8 +7,3 @@ output "cluster_endpoint" {
   description = "EKS cluster API endpoint."
   value       = module.dbnl.cluster_endpoint
 }
-
-output "load_balancer_hostname" {
-  description = "ALB hostname for DNS. After apply run: kubectl get ingress -A -o jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}'"
-  value       = "Run: kubectl get ingress -A -o jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}'"
-}
